3a60a03d1e299bf686f78f6e8f6c0d31d030860d
AhmadIlmifiArkan <ahmadilmifi.arkan@gmail.com>
2021-09-15 09:55:02 +0700

    yahoo

---
+++ app/src/main/java/learn/idn/AbstractionProject/HomeActivity.java
@@ -0,0 +1,6 @@
+package learn.idn.AbstractionProject;
+
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+}
---
+++ app/src/main/java/learn/idn/AbstractionProject/activity_home.java
@@ -0,0 +1,6 @@
+package learn.idn.AbstractionProject;
+
+import android.app.Activity;
+
+public class activity_home extends Activity {
+}
